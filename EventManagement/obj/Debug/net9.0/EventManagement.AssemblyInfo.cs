5c7195b14d572c7e054a89c9b060ab10aa67ac0b
GahinathMadake <gahinathmadake@gmail.com>
2025-05-10 17:24:52 +0530

    Project Completed :)

--- EventManagement/obj/Debug/net9.0/EventManagement.AssemblyInfo.cs
+++ EventManagement/obj/Debug/net9.0/EventManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fa5d3fbe07395ce0b2e05a6287dd1419a271c31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe6507c24a95466537aadc48ecce839eba650bd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
